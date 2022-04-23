dbddf917b5c14a65ab2f31fbc1817c8f68f1fcd0
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-23 23:32:21 +0300

    Middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 // express app
 const app = express()
@@ -8,6 +9,15 @@ app.set('view engine', 'ejs')
 
 // listen for requests
 app.listen(3000)
+
+// middleware & static files
+app.use(express.static('public'))
+app.use(morgan('dev'))
+
+// app.use((req, res, next) => {
+//   console.log('next');
+//   next();
+// })
 
 app.get('/', (req, res) => {
   const blogs = [
