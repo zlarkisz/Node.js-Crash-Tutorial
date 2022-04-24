ff56e9d82a275762cd459b9ff4cc41e840dc7e33
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-24 13:15:00 +0300

    MongoDB

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+const { rearg } = require('lodash')
 
 // express app
 const app = express()
 
+// connetct to mogodb
+const dbRI = 'mongodb+srv://zlarkisz:zlarkisz270397@cluster0.qgcwq.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbRI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.error(err))
+
 //register view engine
 app.set('view engine', 'ejs')
 
-// listen for requests
-app.listen(3000)
 
 // middleware & static files
 app.use(express.static('public'))
 app.use(morgan('dev'))
 
-// app.use((req, res, next) => {
-//   console.log('next');
-//   next();
-// })
-
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-    { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-    { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-  ];
-  res.render('index', { title: 'Home', blogs })
+  res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
   // res.send('<p>About page</p>')
   res.render('about', { title: 'About' })
+})
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch(err => console.log(err))
 })
 
 app.get('/blogs/create', (req, res) => {
