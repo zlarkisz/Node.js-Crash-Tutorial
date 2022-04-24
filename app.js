1dec367bd2e8f9875d0d5968db7ded8cc15d2912
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-24 13:52:30 +0300

    GET, POST, & DELETE Requests

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
 const { rearg } = require('lodash')
+const { render } = require('express/lib/response')
 
 // express app
 const app = express()
@@ -19,6 +20,7 @@ app.set('view engine', 'ejs')
 
 // middleware & static files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
 // routes
@@ -36,6 +38,35 @@ app.get('/blogs', (req, res) => {
   Blog.find().sort({ createdAt: -1 })
     .then(result => {
       res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch(err => console.log(err))
+})
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs')
+    })
+    .catch(err => console.log(err))
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog details' })
+    })
+    .catch(err => console.log(err))
+})
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id
+
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' })
     })
     .catch(err => console.log(err))
 })
